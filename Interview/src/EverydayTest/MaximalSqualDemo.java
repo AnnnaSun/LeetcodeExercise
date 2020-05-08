60ce2d1aef60922eeba77fa7f6e55b9c2c1d47ab
Anna Sun <39726678+AnnnaSun@users.noreply.github.com>
2020-05-08 16:56:49 +0800

    找到最大的正方形面积

---
+++ Interview/src/EverydayTest/MaximalSqualDemo.java
@@ -0,0 +1,71 @@
+package EverydayTest;
+
+/**
+ *在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ */
+public class MaximalSqualDemo {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    // 遇到一个 1 作为正方形的左上角
+                    maxSide = Math.max(maxSide, 1);
+                    // 计算可能的最大正方形边长
+                    int currentMaxSide = Math.min(rows - i, columns - j);
+                    for (int k = 1; k < currentMaxSide; k++) {
+                        // 判断新增的一行一列是否均为 1
+                        boolean flag = true;
+                        if (matrix[i + k][j + k] == '0') {
+                            break;
+                        }
+                        for (int m = 0; m < k; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+
+    /**
+     * 动态方案，更佳选择。
+     * https://leetcode-cn.com/problems/maximal-square/solution/li-jie-san-zhe-qu-zui-xiao-1-by-lzhlyle/
+     */
+
+    public int maximalSquare2(char[][] matrix) {
+        // base condition
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
+
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int maxSide = 0;
+
+        // 相当于已经预处理新增第一行、第一列均为0
+        int[][] dp = new int[height + 1][width + 1];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (matrix[row][col] == '1') {
+                    dp[row + 1][col + 1] = Math.min(Math.min(dp[row + 1][col], dp[row][col + 1]), dp[row][col]) + 1;
+                    maxSide = Math.max(maxSide, dp[row + 1][col + 1]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
