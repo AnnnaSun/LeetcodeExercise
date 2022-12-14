e14983951bd12b8e2764a2848c9d53e0da63a263
AnnaSun <annnasun@outlook.com>
2022-12-15 02:00:38 +0800

    daily

---
+++ Interview/src/EverydayTest/solution12_15.java
@@ -0,0 +1,30 @@
+package src.EverydayTest;
+
+//给你一个由小写字母组成的字符串 s ，以及一个整数 k 。
+//
+//首先，用字母在字母表中的位置替换该字母，将 s 转化 为一个整数（也就是，'a' 用 1 替换，'b' 用 2 替换，... 'z' 用 26 替换）。接着，将整数 转换 为其 各位数字之和 。共重复 转换 操作 k 次 。
+//
+public class solution12_15 {
+    public static int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            sb.append(ch - 'a' + 1);
+        }
+        String digits = sb.toString();
+        for (int i = 1; i <= k && digits.length() > 1; ++i) {
+            int sum = 0;
+            for (int j = 0; j < digits.length(); ++j) {
+                char ch = digits.charAt(j);
+                sum += ch - '0';
+            }
+            digits = Integer.toString(sum);
+        }
+
+        return Integer.parseInt(digits);
+    }
+
+    public static void main(String[] args) {
+        getLucky("iaozzbyqzwbpurzze", 2);
+    }
+}
